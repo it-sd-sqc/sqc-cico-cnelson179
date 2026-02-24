c7fde6d227863446af4a093a231a919ab8966141
unknown <cnelson179@student.cvtc.edu>
2026-02-23 22:13:10 -0600

    Fix build.sh to return javac exit code and update manual test

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
+set -x
 
-pushd src
+pushd src >/dev/null || exit 1
+
 javac edu/cvtc/itsd/Main.java
-popd
+rc=$?
+echo "DEBUG: javac rc=$rc"
+
+popd >/dev/null || exit 1
+echo "DEBUG: exiting rc=$rc"
+exit "$rc"
